39272949776b4f84889901e7d52f174e0b5ed86d
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-12-03 22:23:44 +0800

    feat: implement rudimentary command-entry loop

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,10 +2,54 @@ mod game;
 
 use game::*;
 
+#[derive(Debug)]
+enum Command {
+    Reveal { x: u8, y: u8 },
+    Flag { x: u8, y: u8 },
+    Quit
+}
+
 fn main() {
     let mut game = Game::new(GameDifficulty::EASY);
-    println!("{}", game);
-    game.handle_action(GameAction::REVEAL { x: 0, y: 0 })
-        .unwrap();
-    println!("{}", game);
+
+    println!("Welcome to Multisweeper [test]!");
+
+    loop {
+        println!("{}", game);
+
+        let command = read_command();
+
+        println!("{:?}", command);
+    }
+}
+
+fn read_command() -> Command {
+    println!("'r [x] [y]' to reveal a tile\n'f [x] [y]' to flag a tile\n'q' to quit\nNote that (x, y) input is 1-indexed from top-left");
+    loop {
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+        
+        let Some(char) = input.get(0..1) else {
+            continue;
+        };
+
+        let args: Vec<&str> = input.split(" ").map(|str| str.trim()).collect();
+        println!("{:?}", args);
+        return match char {
+            "r" => {
+                if args.len() != 3 {
+                    continue;
+                }
+                return Command::Reveal { x: args[1].parse().unwrap(), y: args[2].parse().unwrap() }
+            },
+            "f" => {
+                if args.len() != 3 {
+                    continue;
+                }
+                return Command::Flag { x: args[1].parse().unwrap(), y: args[2].parse().unwrap() }
+            },
+            "q" => Command::Quit,
+            _ => continue
+        }
+    }
 }
