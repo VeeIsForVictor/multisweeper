6e726095b20c9fd44d64ccbe9839436c56ecd409
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-12-07 16:56:31 +0800

    feat: setup main server loop

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,16 @@
+use tokio::net::TcpListener;
+
 mod game;
 mod cli_local;
+mod protocol;
 
+#[tokio::main]
 #[tracing::instrument]
-fn main() {
+async fn main() {
     tracing_forest::init();
+    let listener = TcpListener::bind("localhost:8080").await.expect("failed to bind to port");
+
+    while let Ok((stream, _)) = listener.accept().await {
+
+    }
 }
