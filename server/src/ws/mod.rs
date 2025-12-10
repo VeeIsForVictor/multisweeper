cc386ba9acba45e47e1d1211e080d8c308054bf1
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-12-11 00:25:10 +0800

    feat: complete SharedState for lobby data

--- server/src/ws/mod.rs
+++ server/src/ws/mod.rs
@@ -1,8 +1,13 @@
+use std::collections::HashMap;
+use tokio::sync::mpsc;
+
+use crate::ws::lobby::{LobbyCode, LobbyCommand};
+
 mod lobby;
 pub mod protocol;
 
 pub type PlayerId = String;
 
 pub struct SharedState {
-
+    lobbies: HashMap<LobbyCode, mpsc::Sender<LobbyCommand>>
 }
