92be124da0ee9eb2a094d5e028b2c926a43a6cd9
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-12-11 17:19:48 +0800

    feat: implement lobby state-broadcasting method

--- server/src/ws/lobby.rs
+++ server/src/ws/lobby.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc::Receiver;
 
-use crate::ws::{PlayerId, protocol::{LobbyCommand, PlayerConnection}};
+use crate::ws::{PlayerId, protocol::{LobbyCommand, PlayerConnection, ServerMessage}};
 
 pub type LobbyCode = String;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum LobbyStatus {
     Waiting,
     Starting
@@ -29,5 +29,21 @@ impl Lobby {
         lobby.players.insert(host_id, host_connection);
 
         return lobby;
+    }
+
+    pub fn register_player(&mut self, player_id: PlayerId, player_connection: PlayerConnection) -> PlayerId {
+        self.players.insert(player_id.clone(), player_connection);
+        return player_id;
+    }
+
+    pub async fn broadcast_state(&mut self) {
+        let players_list: Vec<PlayerId> = self.players.keys().cloned().collect();
+        for player in self.players.values() {
+            player.message_sdr.send(ServerMessage::LobbyState {
+                players: players_list.clone(), 
+                host_id: self.host_id.clone(), 
+                status: self.status.clone()
+            }).await;
+        }
     }
 }
