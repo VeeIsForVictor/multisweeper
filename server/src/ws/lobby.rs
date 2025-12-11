b70002425da57cf06d0618fae8c311e1b59ee1e8
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-12-11 17:03:35 +0800

    feat: implement lobby constructor

--- server/src/ws/lobby.rs
+++ server/src/ws/lobby.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::Receiver;
 
-use crate::ws::{PlayerId, protocol::PlayerConnection};
+use crate::ws::{PlayerId, protocol::{LobbyCommand, PlayerConnection}};
 
 pub type LobbyCode = String;
 
@@ -11,8 +12,22 @@ pub enum LobbyStatus {
     Starting
 }
 
-struct Lobby {
+pub struct Lobby {
     players: HashMap<PlayerId, PlayerConnection>,
     host_id: PlayerId,
     status: LobbyStatus
+}
+
+impl Lobby {
+    pub fn new(host_id: PlayerId, host_connection: PlayerConnection) -> Self {
+        let mut lobby = Lobby {
+            players: HashMap::new(),
+            host_id: host_id.clone(),
+            status: LobbyStatus::Starting
+        };
+
+        lobby.players.insert(host_id, host_connection);
+
+        return lobby;
+    }
 }
