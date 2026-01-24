161de5d5ba2f7e4899931f0210acb893adb61567
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-01-24 15:23:32 +0800

    feat: set up webpage layout

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -6,4 +6,7 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="flex flex-col items-center justify-center min-h-dvh">
+	{@render children()}
+</div>
