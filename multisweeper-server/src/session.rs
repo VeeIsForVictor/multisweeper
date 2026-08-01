b6e4f3ac052bd2bce88f601776daef2d5ac9d68e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-08-02 04:56:47 +0800

    fix: correct inversion

--- multisweeper-server/src/session.rs
+++ multisweeper-server/src/session.rs
@@ -7,8 +7,8 @@ use crate::protocol::{ServerMessage, ServerResponse};
 pub type PlayerId = String;
 pub type PlayerMailbox = Receiver<ServerMessage>;
 pub type PlayerHandle = Sender<ServerMessage>;
-pub type PlayerOutbound = SplitStream<WebSocketStream<TcpStream>>;
-pub type PlayerInbound = SplitSink<WebSocketStream<TcpStream>, Message>;
+pub type PlayerInbound = SplitStream<WebSocketStream<TcpStream>>;
+pub type PlayerOutbound = SplitSink<WebSocketStream<TcpStream>, Message>;
 
 pub struct Session {
     id: PlayerId,
@@ -21,7 +21,7 @@ pub struct Session {
 impl Session {
     pub fn new(id: PlayerId, stream: WebSocketStream<TcpStream>) -> Self {
         let (sender, receiver) = mpsc::channel(10);
-        let (ws_in, ws_out) = stream.split();
-        return Session { id, mailbox: receiver, handle: sender, outbound: ws_out, inbound: ws_in };
+        let (sink, source) = stream.split();
+        return Session { id, mailbox: receiver, handle: sender, outbound: sink, inbound: source };
     }
 }
